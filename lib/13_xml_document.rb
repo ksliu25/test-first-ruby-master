52ca91d294f461fec18e157d358b8cd3d1d066e8
ksliu25 <ksliu25@gmail.com>
2015-02-07 20:32:57 -0600

    More of that method missing business
    Getting to work with each_pair is awesome. I definitely had to look in
    several different places to wrap my head around but it just feels great
    learning it.

--- lib/13_xml_document.rb
+++ lib/13_xml_document.rb
@@ -1,3 +1,7 @@
+#I did some supplmental reading on http://rubylearning.com/satishtalim/ruby_method_missing.html
+#but also learned most of it from a solution posted online.
+#using an empty hash finally feels a little familiar to me and using each_pair makes a lot of sense.
+
 class XmlDocument
 	attr_accessor :xml
 	
@@ -6,6 +10,11 @@ class XmlDocument
 	end
 	
 	def method_missing(method_name, *args, &block)
+		xml = ""
+		attributes = args[0] || {}
+		attributes == {} ? xml << "<#{method_name}/>" : xml <<  "<#{method_name}" 
+		attributes.each_pair {|key, value| xml << " #{key}='#{value}'/>"}
+		xml
 	end
 	
 	
